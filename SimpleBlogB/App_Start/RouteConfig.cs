185d7d1186e65ba6ab0492937626fbb81080602b
Michael Fortier <Michael Fortier>
2016-05-02 14:23:04 -0600

    Views Commit

--- SimpleBlogB/App_Start/RouteConfig.cs
+++ SimpleBlogB/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlogB.Controllers;
 
 namespace SimpleBlogB
 {
@@ -11,13 +12,13 @@ namespace SimpleBlogB
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] {typeof(PostsController).Namespace};
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"}, namespaces);
+            routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"}, namespaces);
+
         }
     }
 }
