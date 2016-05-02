189091d1e1e38c48ce09756a54c6ea07d7cea526
Michael Fortier <Michael Fortier>
2016-05-02 14:50:39 -0600

    Added \basic login form.

--- SimpleBlogB/Controllers/AuthController.cs
+++ SimpleBlogB/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlogB.ViewModels;
 
 namespace SimpleBlogB.Controllers
 {
@@ -11,7 +12,30 @@ namespace SimpleBlogB.Controllers
         // GET: Auth
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin()
+            {
+                
+            });
+        }
+
+        // POST: Auth
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            if (form.Username == "mike")
+                return Content("Valid");
+
+            ModelState.AddModelError("Username", "Username is not correct");
+
+            return View(form);
+
+
+            
         }
     }
 }
---
+++ SimpleBlogB/ViewModels/Auth.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlogB.ViewModels
+{
+    public class AuthLogin
+    { 
+        [Required]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
