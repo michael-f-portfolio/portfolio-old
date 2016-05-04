7be8ce3b9ce15f174328519a82f513d958214b07
Michael Fortier <Michael Fortier>
2016-05-03 22:08:10 -0600

    Worked on frontend Posts controller; implemented all markup for views

--- SimpleBlogB/App_Start/RouteConfig.cs
+++ SimpleBlogB/App_Start/RouteConfig.cs
@@ -16,6 +16,12 @@ namespace SimpleBlogB
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("TagForRealThisTime", "tag/{idAndSlug}", new {controller = "Posts", action = "Tag"}, namespaces);
+            routes.MapRoute("Tag", "tag/{id}-{slug}", new {controller = "Posts", action = "Tag"}, namespaces);
+
+            routes.MapRoute("PostForRealThisTime", "post/{idAndSlug}", new {controller = "Posts", action = "Show"}, namespaces);
+            routes.MapRoute("Post", "post/{id}-{slug}", new {controller = "Posts", action = "Show"}, namespaces);
+
             routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"}, namespaces);
             routes.MapRoute("Logout", "logout", new { controller = "Auth", action = "Logout" }, namespaces);
             routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"}, namespaces);
---
+++ SimpleBlogB/ViewModels/Posts.cs
@@ -0,0 +1,21 @@
+﻿using SimpleBlogB.Infrastructure;
+using SimpleBlogB.Models;
+
+namespace SimpleBlogB.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsShow
+    {
+        public Post Post { get; set; }
+    }
+
+    public class PostsTag
+    {
+        public Tag Tag { get; set; }
+        public PagedData<Post> Posts { get; set; }
+    }
+}
