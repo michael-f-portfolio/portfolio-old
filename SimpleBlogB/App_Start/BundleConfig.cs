ec81adc7d19148b045d76bd9932f0fefdf0deead
Michael Fortier <Michael Fortier>
2016-05-03 23:01:30 -0600

    Finalized Frontend.

--- SimpleBlogB/App_Start/BundleConfig.cs
+++ SimpleBlogB/App_Start/BundleConfig.cs
@@ -28,9 +28,11 @@ namespace SimpleBlogB
 
             bundles.Add(new ScriptBundle("~/scripts")
                 .Include("~/scripts/jquery-2.2.3.js")
+                .Include("~/scripts/jquery.timeago.js")
                 .Include("~/scripts/jquery.validate.js")
                 .Include("~/scripts/jquery.validate.unobtrusive.js")
-                .Include("~/scripts/bootstrap.js"));
+                .Include("~/scripts/bootstrap.js")
+                .Include("~/scripts/Frontend.js"));
         }
     }
 }
--- SimpleBlogB/App_Start/RouteConfig.cs
+++ SimpleBlogB/App_Start/RouteConfig.cs
@@ -26,6 +26,7 @@ namespace SimpleBlogB
             routes.MapRoute("Logout", "logout", new { controller = "Auth", action = "Logout" }, namespaces);
             routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"}, namespaces);
 
+            routes.MapRoute("Sidebar", "", new {controller = "Layout", action = "Sidebar"}, namespaces);
         }
     }
 }
