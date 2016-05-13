5c50be5673b5634b89266dc052850c510116f8b3
Pajex <mfortier21github@gmail.com>
2016-05-13 15:58:52 -0600

    Added console project to practice more lower-level c# practices.

---
+++ Console/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace MikesConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var node1 = new Node { Text = "x" };
+            //var node2 = new Node { Text = "y" };
+            //var node3 = new Node { Text = "z" };
+
+            //Node[] nodeArray = {node1, node2, node3};
+
+            //TreeNode node = new TreeNode
+            //{
+            //    Nodes = nodeArray,
+            //    Text = "This is the root node"
+            //};
+
+            var node1 = new System.Windows.Forms.TreeNode { Text = "x" };
+            var node2 = new System.Windows.Forms.TreeNode { Text = "y" };
+            var node3 = new System.Windows.Forms.TreeNode { Text = "z" };
+
+            var root = new System.Windows.Forms.TreeNode("ROOT", new []{ node1, node2, node3});
+
+            Foo(root);
+
+        }
+
+        public static void Foo(System.Windows.Forms.TreeNode root)
+        {
+            var stack = new Stack();
+            stack.Push(root);
+
+            Console.WriteLine($"DEBUG : {stack.Count}");
+
+            while (stack.Count > 0)
+            {
+                var node = (TreeNode) stack.Pop();
+                Console.WriteLine($"OUTPUT : {node.Text}");
+
+                for (var i = node.Nodes.Count - 1; i >= 0; i--)
+                {
+                    stack.Push(node.Nodes[i]);
+                }
+            }
+        }
+    }
+
+    //public class TreeNode : Node
+    //{
+    //    public Node[] Nodes { get; set; }
+        
+    //}
+
+    //public class Node 
+    //{
+    //    public virtual string Text { get; set; }
+    //}
+}
--- Portfolio/Scripts/main.js
+++ Portfolio/Scripts/main.js
@@ -1,18 +1,21 @@
 ﻿$(document).ready(function() {
     var words = [
-        'awe inspiring',
-        'exciting',
-        'copywrited',
-        'unoriginal',
-        '0x7A31C7',
-        'plagiarised',
-        'far to complex for you to understand',
-        '[missing text: please ignore]'
+        "awe inspiring",
+        "exciting",
+        "copywrited",
+        "unoriginal",
+        "0x7A31C7",
+        "plagiarised",
+        "far to complex for you to understand",
+        "[missing text: please ignore]",
+        "goddamn awful",
+        "stolen from stackoverflow",
+        ""
     ];
     var i = 0;
 
     setInterval(function() {
-        $('#bullshit').fadeOut(function () {
+        $("#bullshit").fadeOut(function () {
                 $(this).html(words[i = (i + 1) % words.length]).fadeIn();
         });
     }, 5000);
